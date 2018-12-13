619dbc55f4636b632fc3b5a0c8f0f0fecad66310
Moustafa Mohsen <moustafamohsen1@hotmail.com>
2018-12-13 22:47:56 +0200

    set baseurl to heroku

--- src/app/statics/config.ts
+++ src/app/statics/config.ts
@@ -2,5 +2,5 @@
 //export const _BaseUrl = "http://localhost:6291";
 //export const _BaseUrl = "https://localhost:50001";
 //export const _BaseUrl = "http://localhost:5000";
-export const _BaseUrl = "https://linux-docker-5.herokuapp.com";
+export const _BaseUrl = "https://aspnet-1.herokuapp.com";
 //export const _BaseUrl = "https://linux-docker-5.herokuapp.com/api/GroceriesApi";
