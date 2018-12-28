7d3ada4bc424c9449139282fd730fe3ea8ae1d3c
Moustafa Mohsen <moustafamohsen1@gmail.com>
2018-12-28 16:46:25 +0200

    added delete model

--- src/app/components/account-components/edit-user-page/delete-user/delete-user.component.ts
+++ src/app/components/account-components/edit-user-page/delete-user/delete-user.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService, UserService } from '../../../../_auth.collection';
 import { MatSnackBar } from '@angular/material';
 import { Router } from '@angular/router';
+import { HelpersService } from 'src/app/Services/helpers.service';
 
 @Component({
   selector: 'app-delete-user',
@@ -9,18 +10,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./delete-user.component.css']
 })
 export class DeleteUserComponent implements OnInit {
+  TheRandomString: string=this.helper.randomString();
+  removeIdConfirm:string="#"+this.TheRandomString;
+  removeConfirmForId:string=this.TheRandomString;
 
+  btnClicked=false;
   constructor(
     private router: Router,
     private auth:AuthenticationService,
     private user:UserService,
-    private snack:MatSnackBar
+    private snack:MatSnackBar,
+    private helper:HelpersService
     ) { }
 
   ngOnInit() {
   }
   DeleteAccount(){
+    this.btnClicked=true
     this.user.DeleteUser().subscribe(u=>{
+      this.btnClicked=false
       console.log("==DeleteUser()");
       console.log(u);
       console.log("DeleteUser()==");
@@ -32,6 +40,11 @@ export class DeleteUserComponent implements OnInit {
         console.log("navigate");  
         this.router.navigate([""]);
       }
+    },
+    e=>{
+      let error = e.error?e.error.errors:"Delete Error"
+      this.snack.open(`${error}`,"X",{duration:5000})
+      this.btnClicked=false
     })
   }
 }
--- src/app/statics/config.ts
+++ src/app/statics/config.ts
@@ -3,8 +3,5 @@
 //export const _BaseUrl = "https://localhost:44390";
 //export const _BaseUrl = "https://localhost:50001";
 //export const _BaseUrl = "http://localhost:5000";
-var protocol = location.protocol;
-var slashes = protocol.concat("//");
-export const host = slashes.concat(window.location.hostname);
 export const _BaseUrl = "https://aspnet-1.herokuapp.com";
 //export const _BaseUrl = "https://linux-docker-5.herokuapp.com/api/GroceriesApi";
