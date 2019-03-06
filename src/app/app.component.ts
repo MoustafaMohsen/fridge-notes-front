2c8f4d0dd747fcd2668e9207063a2b11799a9b77
Moustafa Mohsen <moustafamohsen1@gmail.com>
2019-03-06 20:23:08 +0200

    better styling, deleted jquery from index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+declare const $:any;
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   template: `
-  <div>
-  <app-topnav></app-topnav>
-  <router-outlet></router-outlet>
-  </div>
-  
+    <div class="container-fluid">
+      <div class="row no-gutters mar--15">
+      <div class="col-12">
+        <app-topnav></app-topnav>
+        <div id="body-container">
+          <router-outlet ></router-outlet>
+        </div>
+      </div>
+      </div>
+    </div>
   `
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = "app";
+  ngOnInit(): void {
+
+    console.log("======Body Hieght")
+    console.log("body-container",)
+    console.log("Body Hieght======")
+  }
+  
 }
--- src/app/statics/config.ts
+++ src/app/statics/config.ts
@@ -1,7 +1,7 @@
 //export const _BaseUrl = "https://linux-docker-4.herokuapp.com/api/GroceriesApi";
 //export const _BaseUrl = "https://localhost:6292";
-//export const _BaseUrl = "https://localhost:44390";
+export const _BaseUrl = "https://localhost:44390";
 //export const _BaseUrl = "https://localhost:50001";
 //export const _BaseUrl = "http://localhost:5000";
-export const _BaseUrl = "https://aspnet-1.herokuapp.com";
+//export const _BaseUrl = "https://aspnet-1.herokuapp.com";
 //export const _BaseUrl = "https://linux-docker-5.herokuapp.com/api/GroceriesApi";
